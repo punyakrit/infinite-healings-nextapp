040fc8e8c863def06992f58ae93f79969266bfbe
Punyakrit <punyakritsinghmakhni@gmail.com>
2024-04-21 19:50:07 +0530

    footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,19 @@
+import Link from 'next/link'
 import React from 'react'
+import { FaWhatsapp } from 'react-icons/fa'
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <div className='bg-gray-900 h-32 text-white flex px-10 justify-between items-center'>
+      <Link href={"https://www.infinitehealings.in/"} className='font-bold text-2xl cursor-pointer'>
+        InfinteHealings.in
+      </Link>
+      <div className='flex flex-col text-xl  items-center'>
+        Socials
+        <Link href={"https://wa.me/9876599987"}>
+          <FaWhatsapp className='text-2xl font-bold cursor-pointer'/>
+        </Link>
+      </div>
+    </div>
   )
 }
--- pages/Home.tsx
+++ pages/Home.tsx
@@ -5,6 +5,7 @@ import Heading from "@/components/Heading";
 import About from "@/components/About";
 import Services from "@/components/Services";
 import Accordian from "@/components/Accordian";
+import Footer from "@/components/Footer";
 
 function Home() {
   return (
@@ -14,6 +15,7 @@ function Home() {
       <About/>
       <Services/>
       <Accordian/>
+      <Footer/>
     </div>
   );
 }
