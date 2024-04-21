fb0d27c3211f8c1926d4030919496714b6c20b4d
Punyakrit <punyakritsinghmakhni@gmail.com>
2024-04-21 20:28:25 +0530

    services

--- pages/Services.tsx
+++ pages/Services.tsx
@@ -1,9 +1,24 @@
+import Card from '@/components/Card'
+import { cardsData } from '@/data'
 import React from 'react'
 
 function Services() {
   return (
-    <div>
-      
+    <div className='bg-gray-900 text-white'>
+      <div className='flex justify-center pt-16 text-2xl font-bold' > Services We Provide</div>
+      <div className="flex justify-center px-3">
+        <div className="md:flex justify-center flex-wrap gap-8 px-4">
+        {cardsData.map((data) => (
+            <Card
+              key={data.id}
+              title={data.title}
+              imageUrl={data.imageUrl}
+              alt={data.alt}
+              description={data.description}
+            />
+          ))}
+        </div>
+      </div>
     </div>
   )
 }
